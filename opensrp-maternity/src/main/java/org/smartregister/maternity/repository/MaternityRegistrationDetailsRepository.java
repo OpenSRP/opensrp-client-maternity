a7d7034308071f213895bf7098736f26c9921712
bennsimon <botieno@ona.io>
2020-07-23 16:45:17 +0300

    fix update registration

--- opensrp-maternity/src/main/java/org/smartregister/maternity/repository/MaternityRegistrationDetailsRepository.java
+++ opensrp-maternity/src/main/java/org/smartregister/maternity/repository/MaternityRegistrationDetailsRepository.java
@@ -3,14 +3,18 @@ package org.smartregister.maternity.repository;
 import android.support.annotation.NonNull;
 
 import org.apache.commons.lang3.StringUtils;
+import org.smartregister.maternity.MaternityLibrary;
 import org.smartregister.maternity.utils.MaternityDbConstants;
+import org.smartregister.maternity.utils.MaternityUtils;
 import org.smartregister.repository.BaseRepository;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import timber.log.Timber;
 
 public class MaternityRegistrationDetailsRepository extends BaseRepository {
+
     public HashMap<String, String> findByBaseEntityId(@NonNull String baseEntityId) {
         try {
             if (StringUtils.isNotBlank(baseEntityId)) {
@@ -18,6 +22,18 @@ public class MaternityRegistrationDetailsRepository extends BaseRepository {
                         "select * from " + MaternityDbConstants.Table.MATERNITY_REGISTRATION_DETAILS +
                                 " where " + MaternityDbConstants.Column.MaternityDetails.BASE_ENTITY_ID + " = '" + baseEntityId + "' limit 1").get(0);
             }
+        } catch (NullPointerException | IndexOutOfBoundsException e) {
+            Timber.e(e);
+        }
+        return null;
+    }
+
+    public Map<String, String> getClientDemographicDetails(@NonNull String baseEntityId) {
+        try {
+            return MaternityLibrary.getInstance().context().getEventClientRepository()
+                    .rawQuery(MaternityLibrary.getInstance().context().getEventClientRepository().getReadableDatabase(),
+                            "select * from " + MaternityUtils.metadata().getTableName() +
+                                    " where " + MaternityDbConstants.Column.Client.BASE_ENTITY_ID + " = '" + baseEntityId + "' limit 1").get(0);
         } catch (NullPointerException | IndexOutOfBoundsException e) {
             Timber.e(e);
         }
--- opensrp-maternity/src/main/java/org/smartregister/maternity/tasks/FetchRegistrationDataTask.java
+++ opensrp-maternity/src/main/java/org/smartregister/maternity/tasks/FetchRegistrationDataTask.java
@@ -5,7 +5,7 @@ import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import org.smartregister.CoreLibrary;
+import org.smartregister.maternity.MaternityLibrary;
 import org.smartregister.maternity.utils.MaternityConstants;
 import org.smartregister.maternity.utils.MaternityJsonFormUtils;
 import org.smartregister.maternity.utils.MaternityReverseJsonFormUtils;
@@ -19,14 +19,14 @@ public class FetchRegistrationDataTask extends AsyncTask<String, Void, String> {
     private WeakReference<Context> contextWeakReference;
     private OnTaskComplete onTaskComplete;
 
-    public FetchRegistrationDataTask(@NonNull WeakReference<Context> contextWeakReference, @NonNull OnTaskComplete onTaskComplete){
+    public FetchRegistrationDataTask(@NonNull WeakReference<Context> contextWeakReference, @NonNull OnTaskComplete onTaskComplete) {
         this.contextWeakReference = contextWeakReference;
         this.onTaskComplete = onTaskComplete;
     }
 
     @Nullable
     protected String doInBackground(String... params) {
-        Map<String, String> detailsMap = CoreLibrary.getInstance().context().detailsRepository().getAllDetailsForClient(params[0]);
+        Map<String, String> detailsMap = MaternityLibrary.getInstance().getMaternityRegistrationDetailsRepository().getClientDemographicDetails(params[0]);
         detailsMap.put(MaternityJsonFormUtils.OPENSRP_ID, detailsMap.get(MaternityConstants.KEY.OPENSRP_ID));
         return MaternityReverseJsonFormUtils.prepareJsonEditMaternityRegistrationForm(detailsMap, Arrays.asList(MaternityJsonFormUtils.OPENSRP_ID, MaternityConstants.JSON_FORM_KEY.BHT_ID), contextWeakReference.get());
     }
