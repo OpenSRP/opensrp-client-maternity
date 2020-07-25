ff03dd96f2c6d577d44513b19c4a07805aa4cb5d
syedowaisali <12345>
2020-07-25 17:18:04 +0500

    :heavy_check_mark: added test case

---
+++ opensrp-maternity/src/main/java/org/smartregister/maternity/helper/LibraryHelper.java
@@ -0,0 +1,10 @@
+package org.smartregister.maternity.helper;
+
+import org.smartregister.maternity.MaternityLibrary;
+
+public class LibraryHelper {
+
+    public MaternityLibrary getMaternityLibraryInstance() {
+        return MaternityLibrary.getInstance();
+    }
+}
---
+++ opensrp-maternity/src/main/java/org/smartregister/maternity/helper/TextUtilHelper.java
@@ -0,0 +1,10 @@
+package org.smartregister.maternity.helper;
+
+import android.text.TextUtils;
+
+public class TextUtilHelper {
+
+    public boolean isEmpty(CharSequence charSequence) {
+        return TextUtils.isEmpty(charSequence);
+    }
+}
