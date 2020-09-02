e94441b21352e40ccc8749760d799d9d034b2827
bennsimon <botieno@ona.io>
2020-09-02 10:56:11 +0300

    code cleanup

--- opensrp-maternity/src/main/java/org/smartregister/maternity/utils/MaternityUtils.java
+++ opensrp-maternity/src/main/java/org/smartregister/maternity/utils/MaternityUtils.java
@@ -17,6 +17,8 @@ import android.widget.TextView;
 import com.vijay.jsonwizard.constants.JsonFormConstants;
 import com.vijay.jsonwizard.domain.Form;
 
+import net.sqlcipher.SQLException;
+
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.text.WordUtils;
@@ -428,26 +430,34 @@ public class MaternityUtils extends org.smartregister.util.Utils {
         return "";
     }
 
-    public static void updateLastInteractedWith(@NonNull String baseEntityId) {
+    public static void updateLastInteractedWith(@Nullable String baseEntityId) {
         try {
-            String tableName = metadata().getTableName();
+            if (StringUtils.isNotBlank(baseEntityId)) {
+                String tableName = metadata().getTableName();
 
-            String lastInteractedWithDate = String.valueOf(new Date().getTime());
+                String lastInteractedWithDate = String.valueOf(new Date().getTime());
 
-            ContentValues contentValues = new ContentValues();
-            contentValues.put(MaternityConstants.JSON_FORM_KEY.LAST_INTERACTED_WITH, lastInteractedWithDate);
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(MaternityConstants.JSON_FORM_KEY.LAST_INTERACTED_WITH, lastInteractedWithDate);
 
-            MaternityLibrary.getInstance().getRepository().getWritableDatabase()
-                    .update(tableName, contentValues, "base_entity_id = ?", new String[]{baseEntityId});
-
-            // Update FTS
-            CommonRepository commonrepository = MaternityLibrary.getInstance().context().commonrepository(tableName);
-
-            if (commonrepository.isFts()) {
                 MaternityLibrary.getInstance().getRepository().getWritableDatabase()
-                        .update(CommonFtsObject.searchTableName(tableName), contentValues, CommonFtsObject.idColumn + " = ?", new String[]{baseEntityId});
+                        .update(tableName, contentValues,
+                                String.format("%s = ?", MaternityConstants.KEY.BASE_ENTITY_ID),
+                                new String[]{baseEntityId});
+
+                // Update FTS
+                CommonRepository commonrepository = MaternityLibrary
+                        .getInstance()
+                        .context()
+                        .commonrepository(tableName);
+
+                if (commonrepository.isFts()) {
+                    MaternityLibrary.getInstance().getRepository().getWritableDatabase()
+                            .update(CommonFtsObject.searchTableName(tableName), contentValues,
+                                    CommonFtsObject.idColumn + " = ?", new String[]{baseEntityId});
+                }
             }
-        } catch (Exception e) {
+        } catch (SQLException | IllegalArgumentException e) {
             Timber.e(e);
         }
     }
